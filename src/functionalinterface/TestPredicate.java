54b15ae9105e0099a6e39f915e9eedf4ff6857e0
gachaukingori <victor.kingori93@gmail.com>
2023-02-28 23:09:53 +0300

    java streams using the functional interface

--- src/functionalinterface/TestPredicate.java
+++ src/functionalinterface/TestPredicate.java
@@ -1,17 +1,25 @@
 package functionalinterface;
 
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class TestPredicate {
 
     public static void main(String[] args) {
         List<Integer> myList = List.of(21,82,3,20,8, 92);
-//        Predicate<Integer> predicate = myList.stream().filter()
+        Predicate <Integer> predicate = x->x>30;
+        Consumer<Integer> consumer = integer->{
+
+                System.out.println(integer);
+
+        };
+
+
         myList
                 .stream()
-                .filter(integer -> integer>30)
-                .forEach(System.out::println);
+                .filter(predicate)
+                .forEach(consumer);
     }
 
 
---
+++ src/streams/_Streams.java
@@ -0,0 +1,33 @@
+package streams;
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class _Streams {
+    public static void main(String[] args) {
+        List<Integer> myList = List.of(21,82,3,20,8, 92);
+
+        List<Integer>  multiply2 = myList.stream()
+                .map((integer -> integer * 2)).toList();
+
+
+        // contain only even numbers
+
+        Predicate<Integer>  evenPredicate =  integer -> integer%2==0;
+        boolean containsOnlyEven = multiply2.stream()
+                .allMatch(evenPredicate);
+
+
+
+        System.out.println("contains only even " + containsOnlyEven);
+
+        boolean containsAtleastOneEven = myList.stream()
+                .anyMatch(evenPredicate);
+        System.out.println("contains atleast one even " + containsAtleastOneEven);
+
+        boolean containsNoEven = myList.stream()
+                .noneMatch(evenPredicate);
+        System.out.println("contains no even " + containsNoEven);
+    }
+}
